bcdc8c98beb49b6f8822072d3953e406cf25474e
helgenius <helgenius@gmail.com>
2024-08-13 22:53:03 +0200

    fjfghjfhg

--- docs/.vitepress/theme/index.ts
+++
@@ -1,8 +0,0 @@
-import OnyxTheme from "@sit-onyx/vitepress-theme";
-
-// if you use/override custom styles, they must be imported after the theme, e.g.
-// import "./styles.css";
-
-export default OnyxTheme;
-
-import "./style.wut.css";
--- docs/index.md
+++ docs/index.md
@@ -1,5 +1,5 @@
 ---
-# https://vitepress.dev/reference/default-theme-home-page
+# hello
 layout: home
 
 hero:
